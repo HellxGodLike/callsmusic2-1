126cb2fbfc74f04c1ef53098c7221d23dd4282f0
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-17 00:35:28 +0530

    Update index.js

--- bot/handlers/index.js
+++ bot/handlers/index.js
@@ -2,7 +2,6 @@ const play = require("./play");
 const controls = require("./controls");
 const voiceChatEnded = require("./voice_chat_ended");
 
-
 module.exports = (bot) => {
     bot.filter((ctx) => {
         if (ctx.chat?.type === "supergroup") {
@@ -17,6 +16,5 @@ module.exports = (bot) => {
         return false;
     }).use(play);
     bot.filter((ctx) => ctx.chat?.type === "supergroup", controls);
-    bot.filter((ctx) => ctx.chat?.type === "private", privateChat);
     bot.use(voiceChatEnded);
 };
